45c4c386b3d8d00e65138b0e8a932d6dedb0161d
Jack Rosen <jrosen081@gmail.com>
2022-12-09 12:32:49 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["Intercom"]),
         .library(
             name: "Sentry",
-            targets: ["Sentry"])
+            targets: ["Carthage"])
     ],
     targets: [
         .binaryTarget(
@@ -18,8 +18,9 @@ let package = Package(
             url: "https://github.com/intercom/intercom-ios/releases/download/12.2.1/Intercom.xcframework.zip",
             checksum: "705469f671e803e74a30e51ae00be5eade2377bac3f789cacb684cb594d0f6a6"
         ),
+        // The target is `Carthage/Build/Sentry.xcframework`, so this is really sentry
         .binaryTarget(
-            name: "Sentry",
+            name: "Carthage",
             url: "https://github.com/getsentry/sentry-cocoa/releases/download/7.31.3/Sentry.xcframework.zip",
             checksum: "73bbf75e36b29336f3c6a78eb6e904b344327f435cc7538b1e72365536a73e51"
         )
