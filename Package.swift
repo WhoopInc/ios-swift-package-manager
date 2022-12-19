bff8d3c39d89cd1222bdf7d276dccc4c5d437f0b
Nathan Beaumont <nathan.beaumont@whoop.com>
2022-12-19 15:32:06 -0500

    Remove spaces from Package Name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Whoop External Swift Packages",
+    name: "WhoopExternalSwiftPackages",
     products: [
         .library(
             name: "Intercom",
